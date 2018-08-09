daee1f286937f602bf65890cdc48af4a44fbbef5
Adrian Adriano <adrian.adriano@hyraiq.com>
2018-08-09 14:09:02 +0800

    make value converters available globally

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,3 +1,9 @@
 export function configure(config) {
-  //config.globalResources([]);
+  config.globalResources([
+      PLATFORM.moduleName('./exclamation-value-converter'),
+      PLATFORM.moduleName('./json-value-converter'),
+      PLATFORM.moduleName('./reverse-string-value-converter'),
+      PLATFORM.moduleName('./reverse-value-converter'),
+      PLATFORM.moduleName('./upper-case-value-converter'),
+  ]);
 }
---
+++ src/resources/reverse-value-converter.js
@@ -0,0 +1,5 @@
+export class ReverseValueConverter {
+    toView(value) {
+        return value.reverse();
+    }
+}
